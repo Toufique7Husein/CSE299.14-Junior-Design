c2b773cfcca948a425da229d8eea93fe710afa1a
Md Toufique Husein <tfq55390@gmail.com>
2021-12-11 04:46:30 +0600

    user costume authentication done

--- nsu_book_room/short_story/froms.py
+++ nsu_book_room/short_story/froms.py
@@ -1,10 +1,16 @@
 from django.forms import ModelForm
-
 from .models import ShortStory
+from django import forms
 
 class ShortStoryFrom(ModelForm):
     class Meta:
         model = ShortStory
-        fields = '__all__'
+        fields = ['title', 'context']
+       
+        widgets = {
+            'title' : forms.TextInput(attrs={'class':'form-control'}),
+            'context' : forms.Textarea(attrs={'class' : 'form-control'}),
+        }
+        
         
     
